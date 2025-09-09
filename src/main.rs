8ca29f3eb932f25b44661940ca2b3fe527f98b2d
itsara <khajer@gmail.com>
2025-09-09 18:57:39 +0700

    remove print

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ use tokio::net::TcpStream;
 async fn main() -> io::Result<()> {
     // Connect to server
     let mut stream = TcpStream::connect("127.0.0.1:9090").await?;
-    // println!("Connected to server!");
-    let mut first_message = true;
 
+    let mut first_message = true;
     // Send message
     let mut buffer = [0; 4096];
     loop {
         let n = stream.read(&mut buffer).await?;
         if n == 0 {
-            println!("Server closed connection.");
+            println!("Server Closed Connection.");
             break;
         }
 
@@ -26,42 +25,39 @@ async fn main() -> io::Result<()> {
         }
 
         let raw_request = rec_msg.to_string();
-        let mut stream_client = TcpStream::connect("localhost:3000").await?;
 
-        // send request client
-        stream_client.write_all(raw_request.as_bytes()).await?;
-        stream_client.flush().await?;
+        match TcpStream::connect("localhost:3000").await {
+            Ok(stream_local) => {
+                let mut stream_client = stream_local;
+                println!("Successfully connected to localhost:3000");
 
-        // Read response client
-        // let mut full_buffer = Vec::new();
-        // stream_client.read_to_end(&mut full_buffer).await?;
+                stream_client.write(raw_request.as_bytes()).await?;
 
-        let mut full_buffer = vec![0u8; 4096]; // Initial capacity
-        let mut total_data = Vec::new();
-        loop {
-            let n = stream_client.read(&mut full_buffer).await?;
-            if n == 0 {
-                break; // EOF
+                let mut full_buffer = vec![0u8; 4096]; // Initial capacity
+                let mut total_data = Vec::new();
+                loop {
+                    let n = stream_client.read(&mut full_buffer).await?;
+                    if n == 0 {
+                        break; // EOF
+                    }
+                    total_data.extend_from_slice(&full_buffer[..n]);
+
+                    if total_data.windows(4).any(|w| w == b"\r\n\r\n") {
+                        // println!("Complete HTTP headers received");
+                        break;
+                    }
+                }
+                if let Err(e) = stream.write(&full_buffer).await {
+                    eprintln!("Error sending direct message to server: {}", e);
+                }
+                if let Err(e) = stream.flush().await {
+                    eprintln!("Error flushing TCP stream: {}", e);
+                }
             }
-            total_data.extend_from_slice(&full_buffer[..n]);
-
-            if total_data.windows(4).any(|w| w == b"\r\n\r\n") {
-                println!("Complete HTTP headers received");
-                break;
+            Err(e) => {
+                eprintln!("Failed to connect: {}", e);
             }
-        }
-        let s = match String::from_utf8(total_data) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
         };
-        println!("{s}");
-
-        if let Err(e) = stream.write(&full_buffer).await {
-            eprintln!("Error sending direct message to server: {}", e);
-        }
-        if let Err(e) = stream.flush().await {
-            eprintln!("Error flushing TCP stream: {}", e);
-        }
     }
 
     Ok(())
