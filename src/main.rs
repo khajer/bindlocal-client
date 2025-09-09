8b3fc0ea17564c8a37f7eb454a50d08f64ff50f4
itsara <khajer@gmail.com>
2025-09-09 08:47:31 +0700

    chang use lib reqwest to tokio:tcp

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,32 @@ async fn main() -> io::Result<()> {
         stream_client.flush().await?;
 
         // Read response client
-        let mut full_buffer = Vec::new();
-        stream_client.read_to_end(&mut full_buffer).await?;
+        // let mut full_buffer = Vec::new();
+        // stream_client.read_to_end(&mut full_buffer).await?;
+
+        let mut full_buffer = vec![0u8; 4096]; // Initial capacity
+        let mut total_data = Vec::new();
+        loop {
+            println!("start");
+            let n = stream_client.read(&mut full_buffer).await?;
+            println!("read");
+            if n == 0 {
+                break; // EOF
+            }
+            println!("loop");
+            total_data.extend_from_slice(&full_buffer[..n]);
+            println!("extend");
+
+            if total_data.windows(4).any(|w| w == b"\r\n\r\n") {
+                println!("Complete HTTP headers received");
+                break;
+            }
+        }
+        let s = match String::from_utf8(total_data) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        println!("{s}");
 
         if let Err(e) = stream.write(&full_buffer).await {
             eprintln!("Error sending direct message to server: {}", e);
